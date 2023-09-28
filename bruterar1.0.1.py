519edda31f506a1e131a12a63ffbcfb36f646476
RedReplica <145266937+RedReplicaOfficial@users.noreply.github.com>
2023-09-28 16:47:38 +0200

    bruterar1.0.2.py
    Added some QoL changes:
    
    - Once you hit "Start", the clock will start.
    - Added a counter how much password has been tried yet
    - Added Average Tries/Min counter
    - Added Estimated Avg. Tries/hour.
    
    Found bugs, will fix later:
    
    -Pressing start without selecting a good .rar file will mess up the software. 
    
    What will I do:
    
    1. A "Pause" button will be added, which you can guess, will pause the software.
    2. While the software is paused, the "Pause" button will turn into "Resume", and you guessed it, the software will resume working where it was halted.
    3. Once you start cracking a .RAR file, the "Start" button will be greyed out.
    4. Will implement several error-handling regarding user-mistakes

--- bruterar1.0.1.py
+++ bruterar1.0.1.py
@@ -1,25 +1,45 @@
-import rarfile 
-import itertools 
-import os 
-import threading 
-import time
-from tkinter import filedialog, messagebox, Label, Button, Text, Tk, StringVar, END 
+# The usual/dependencies
+import os
+from time import time, strftime, gmtime
+from threading import Thread
+from tkinter import Tk, StringVar, Text, Button, Label, filedialog, END, messagebox
+import itertools
+import rarfile
+import webbrowser
 
-charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
-max_length = 14
-log_interval = 100
+# Set the path to the UnRAR tool
+rarfile.UNRAR_TOOL = r"C:\Program Files\WinRAR\UnRAR.exe"
 
 root = Tk()
 root.title("RR's Simple BruteRAR v1.0.0")
 
-rar_file_path = StringVar()
+charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+max_length = 3
+rar_file_path = StringVar(root)
 log_text = Text(root)
-
-rarfile.UNRAR_TOOL = r"C:\Program Files\WinRAR\UnRAR.exe"
+label_text = StringVar(root)
 
 start_time = 0
 total_tries = 0
-countdown_time = StringVar()
+status_updater_interval = 0.5  # Updated to 0.5 seconds
+clock_updater_interval = 0.5  # Updated to 0.5 seconds
+password_found = False  # Track if the password is found
+
+# Function to open the GitHub link
+def open_github_link():
+    webbrowser.open("https://github.com/RedReplicaOfficial")
+
+# Function to stop the clock and counters
+def stop_counters():
+    global status_updater_interval, clock_updater_interval
+    status_updater_interval = 0  # Stop status updater
+    clock_updater_interval = 0  # Stop clock updater
+
+# Add the text at the top and set it as a clickable label with warning text
+header_text = "Made by Red Replica. Check out other useful software @ https://github.com/RedReplicaOfficial"
+header_label = Label(root, text=header_text + "\n\nWarning: Your browser will open at the GitHub page if you click here!", cursor="hand2")
+header_label.pack(padx=10, pady=10)
+header_label.bind("<Button-1>", lambda e: open_github_link())
 
 def browse_button():
     filename = filedialog.askopenfilename(filetypes=[('RAR files', '*.rar')])
@@ -28,7 +48,7 @@ def browse_button():
         log_text.see(END)
         return
     elif not rarfile.is_rarfile(filename):
-        log_text.insert(END, "This is not a .RAR file.\n")
+        log_text.insert(END, f"This is not a .rar file: {filename}\n")
         log_text.see(END)
         return
     elif not rarfile.RarFile(filename).needs_password():
@@ -37,74 +57,71 @@ def browse_button():
         return
     rar_file_path.set(filename)
 
-def try_password(password):
-    global total_tries
-    if not os.path.exists(rar_file_path.get()):
-        log_text.insert(END, "RAR file does not exist.\n")
+def safe_log_insert(text):
+    if root.state() != "destroyed":
+        log_text.insert(END, text)
         log_text.see(END)
+
+def try_password(password):
+    global total_tries, password_found
+    if not os.path.exists(rar_file_path.get()):
+        safe_log_insert("RAR file does not exist.\n")
         return False
     try:
         with rarfile.RarFile(rar_file_path.get()) as archive:
             archive.extractall(pwd=password)
+            password_found = True
+            stop_counters()  # Stop the counters when password is found
             return True
     except rarfile.BadRarFile:
         total_tries += 1
-        log_text.insert(END, f"Attempted password: {password}\n")
-        log_text.see(END)
-        if int(log_text.index('end-1c').split('.')[0]) > 1000: 
+        safe_log_insert(f"Attempted password: {password}\n")
+        if int(log_text.index('end-1c').split('.')[0]) > 1000:
             log_text.delete('1.0', '2.0')
         return False
 
 def brute_force_password():
     global start_time, total_tries
-    log_counter = 0
-    start_time = time.time()
     for length in range(1, max_length + 1):
         password_combinations = itertools.product(charset, repeat=length)
         for password_tuple in password_combinations:
             password = ''.join(password_tuple)
             if try_password(password):
-                elapsed_time = time.time() - start_time
-                elapsed_time_str = time.strftime("%H:%M:%S", time.gmtime(elapsed_time))
-                speed = total_tries / (elapsed_time / 60)
-                messagebox.showinfo("Password Cracked!", f"File has been cracked! Password is: {password}. It took {elapsed_time_str} Tries per minute: {speed:.2f}")
+                elapsed_time = time() - start_time
+                elapsed_time_str = strftime("%d:%H:%M:%S", gmtime(elapsed_time))
+                average_speed = total_tries / elapsed_time if elapsed_time > 0 else 0
+                messagebox.showinfo("Password Cracked!", f"Password found! Password: {password}. Total time taken to crack: {elapsed_time_str}. Average tries/second: {average_speed:.2f}")
                 return
-            log_counter += 1
-            if log_counter >= log_interval:
-                log_text.insert(END, f"Logged {log_interval} password attempts.\n")
-                log_counter = 0
 
-def thread_it():
-    t = threading.Thread(target=brute_force_password)
-    t.start()
+def times_up():
+    global start_time, total_tries
+    if not password_found:
+        elapsed_time = time() - start_time
+        average_tries_per_second = total_tries / elapsed_time if elapsed_time > 0 else 0
+        average_tries_per_hour = average_tries_per_second * 3600
+        elapsed_time_str = strftime("%H:%M:%S", gmtime(elapsed_time))
 
-def update_label(dt):
-    countdown_time.set(time.strftime("%H:%M:%S", time.gmtime(dt)))
+        # Create a formatted string to display next to the clock
+        display_text = (
+            f"{elapsed_time_str}     |     Tries: {total_tries}     |     "
+            f"Avg Tries/Min: {average_tries_per_second * 60:.2f}     |     "
+            f"Est. Avg Tries/Hour: {average_tries_per_hour:.2f}"
+        )
 
-def start_action():
-    log_text.delete(1.0, END)
-    log_text.insert(END, "Cracking in progress.\n")
-    start_time = int(time.mktime(time.gmtime()))
-    thread_it()
-    update_timer(start_time)
+        label_text.set(display_text)
 
-def update_timer(start_time):
-    dt = int(time.mktime(time.gmtime())) - start_time
-    update_label(dt)
-    root.after(1000, update_timer, start_time)  
+        if root.state() != "destroyed":
+            root.after(int(status_updater_interval * 1000), times_up)  # Updated interval
 
-info_text = "Made by Red Replica. Check out other useful softwares @ https://github.com/RedReplicaOfficial"
-text_widget = Text(root, height=2, width=len(info_text))
-text_widget.insert(1.0, info_text)
-text_widget.configure(state='disabled', relief='flat')
-text_widget.tag_configure("center", justify='center')
-text_widget.tag_add("center", 1.0, "end")
-text_widget.pack()
-Button(text="Browse", command=browse_button).pack()
-Button(text="Start", command=start_action).pack()
+def start_brute_force():
+    global start_time
+    start_time = time()
+    Thread(target=brute_force_password).start()
+    times_up()
 
-Label(root, textvariable=countdown_time).pack() 
-
+Button(root, text="Browse", command=browse_button).pack()
+Button(root, text="Start", command=start_brute_force).pack()
+Label(root, textvariable=label_text).pack()
 Label(root, textvariable=rar_file_path, width=100).pack()
 log_text.pack()
 
