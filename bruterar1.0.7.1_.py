da9e4743541d778c033c25934c73cb7ab8a55ecd
RedReplica <145266937+RedReplicaOfficial@users.noreply.github.com>
2023-10-26 23:11:59 +0200

    Update bruterar1.0.7.1_.py
    New GUI tryout + trying to fix problems

--- bruterar1.0.7.1_.py
+++ bruterar1.0.7.1_.py
@@ -2,9 +2,8 @@ import os
 import itertools
 import rarfile
 import webbrowser
-from threading import Thread
+import threading
 from time import gmtime, strftime, time, sleep
-from tkinter import messagebox
 import PySimpleGUI as sg
 
 rarfile.UNRAR_TOOL = os.path.join(os.environ['ProgramFiles'], "WinRAR", "UnRAR.exe")
@@ -19,11 +18,6 @@ status_updater_interval = 0.5
 clock_updater_interval = 0.5
 password_found = False
 pause = False
-rar_file_path = ''
-
-
-def open_github_link():
-    webbrowser.open("https://github.com/RedReplicaOfficial")
 
 
 def stop_counters():
@@ -33,28 +27,18 @@ def stop_counters():
 
 
 def browse_button():
-    try:
-        filename = sg.popup_get_file('Select RAR file.', file_types=(("RAR files", "*.rar"),))
-        if not filename:
-            return "No file selected."
-        elif not rarfile.is_rarfile(filename):
-            return f"This is not a .rar file"
-        elif not rarfile.RarFile(filename).needs_password():
-            return "RAR file has no password to crack."
-        else:
-            return f"Selected file: {filename}"
-    except Exception as e:
-        return "Error while getting file: " + str(e)
+    filename = sg.popup_get_file('Select RAR file.', file_types=(("RAR files", "*.rar"),))
+    if not filename:
+        return "No file selected."
+    elif not rarfile.is_rarfile(filename):
+        return f"This is not a .rar file"
+    elif not rarfile.RarFile(filename).needs_password():
+        return "RAR file has no password to crack."
+    else:
+        return f"Selected file: {filename}"
 
 
-def safe_log_insert(window, text):
-    try:
-        window['-OUTPUT-'].update(text+'\n', append=True)
-    except Exception:
-        pass
-
-
-def try_password(password):
+def try_password(password, rar_file_path):
     global total_tries, password_found, pause
     while pause:
         sleep(0.1)
@@ -81,18 +65,18 @@ def display_password_found(password):
     return f"Password found! {password}"
 
 
-def brute_force_password(window):
+def brute_force_password(window, rar_file_path):
     global start_time, total_tries
     for length in range(1, max_length + 1):
         password_combinations = itertools.product(charset, repeat=length)
         for password_tuple in password_combinations:
             password = ''.join(password_tuple)
-            result = try_password(password)
+            result = try_password(password, rar_file_path)
             if result == True:
-                safe_log_insert(window, display_password_found(password))
+                print(display_password_found(password))  # Use print() for debugging
                 break
             else:
-                safe_log_insert(window, result)
+                print(result)  # Use print() for debugging
 
 
 def times_up(window):
@@ -109,29 +93,28 @@ def times_up(window):
             f"Avg Tries/Min: {average_tries_per_second * 60:.2f} | "
             f"Est. Avg Tries/Hour: {average_tries_per_hour:.2f}"
         )
-
         window['elapsed'].update(display_text)
-
         if window:
             window.refresh()
             window.after(int(status_updater_interval * 1000), times_up, window)
 
 
-def start_brute_force(window):
-    global start_time, pause_time, rar_file_path
+def start_brute_force(window, rar_file_path):
+    global start_time, pause_time, pause
     pause_time = 0  # reset pause time on each start
     start_time = time()
-    rar_file_path = window['-INPUT-'].get()  # Set the file path from the input field
+    pause = False
     window['Start'].update(disabled=True)
     window['Pause'].update(disabled=False)
-    Thread(target=brute_force_password, args=(window,)).start()
+    threading.Thread(target=brute_force_password, args=(window, rar_file_path), daemon=True).start()
     times_up(window)
 
 
-def pause_brute_force():
+def pause_brute_force(window):
     global pause, pause_start_time
     pause = True
     pause_start_time = time() # Store pause start time
+    window['Resume'].update(disabled=False)
 
 
 def resume_brute_force(window):
@@ -142,7 +125,7 @@ def resume_brute_force(window):
     times_up(window)
 
 
-sg.theme('Dark Blue 3')  # Set your colour
+sg.theme('Dark Blue 3')  # Set theme
 
 # GUI Layout
 layout = [[sg.Text('RRs Simple BruteRAR v1.0.3')],
@@ -159,16 +142,22 @@ while True:
     event, values = window.read()
     if event == sg.WIN_CLOSED or event == "Exit":
         break
-    elif event == '-INPUT-':
-        output = browse_button()
-        rar_file_path = values['-INPUT-']
-        safe_log_insert(window, output)
     elif event == 'Start':
-        start_brute_force(window)
+        rar_file_path = values['-INPUT-']
+        if not rar_file_path or not os.path.exists(rar_file_path):
+            sg.popup('Select a RAR file first!')
+        else:
+            start_brute_force(window, rar_file_path)
     elif event == 'Pause':
-        pause_brute_force()
-        window['Resume'].update(disabled=False)
+        pause_brute_force(window)
     elif event == 'Resume':
         resume_brute_force(window)
+    elif event == '-THREAD-':         # Thread has completed
+        window.write_event_value('-UPDATE-', values[event])
+        window['Start'].update(disabled=False)
+        window['Pause'].update(disabled=True)
+    elif event == '-UPDATE-':        # Thread wants to communicate
+        window['-OUTPUT-'].update(values[event]+'\n', append=True)
+
 
 window.close()
